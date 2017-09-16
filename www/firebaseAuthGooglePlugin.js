fe39c3d9f57e30cf096bfc026cd4488a5ea10af2
Vlado Velichkovski <stormwin@users.noreply.github.com>
2017-09-16 05:39:45 +0200

    Update firebaseAuthGooglePlugin.js

--- www/firebaseAuthGooglePlugin.js
+++ www/firebaseAuthGooglePlugin.js
@@ -6,35 +6,35 @@ function dispatchEvent(event) {
     window.dispatchEvent(new CustomEvent(event.type, {detail: event.data}));
 }
 
-function FirebaseAuth() { }
+function FirebaseAuthGoogle() { }
 
-FirebaseAuth.prototype.init = function(options) {
+FirebaseAuthGoogle.prototype.init = function(options) {
     options = options || {};
     var allowDomains = options.allowDomains ? [].concat(options.allowDomains) : null;
     exec(dispatchEvent, null, PLUGIN_NAME, 'initialize', [allowDomains]);
 };
 
-FirebaseAuth.prototype.getToken = function(success, failure) {
+FirebaseAuthGoogle.prototype.getToken = function(success, failure) {
     
     return exec(success, failure, PLUGIN_NAME, 'getToken', []);
 };
 
-FirebaseAuth.prototype.signIn = function (silent) {
+FirebaseAuthGoogle.prototype.signIn = function (silent) {
 
     return exec(null, null, PLUGIN_NAME, 'signIn', [silent === true]);
 };
 
-FirebaseAuth.prototype.signOut = function () {
+FirebaseAuthGoogle.prototype.signOut = function () {
 
     return exec(null, null, PLUGIN_NAME, 'signOut', []);
 };
 
-FirebaseAuth.prototype.getUserData = function(success, failure) {
+FirebaseAuthGoogle.prototype.getUserData = function(success, failure) {
     
     return exec(success, failure, PLUGIN_NAME, 'getUserData', []);
 };
 
 if (typeof module !== undefined && module.exports) {
 
-    module.exports = new FirebaseAuth();
+    module.exports = new FirebaseAuthGoogle();
 }
